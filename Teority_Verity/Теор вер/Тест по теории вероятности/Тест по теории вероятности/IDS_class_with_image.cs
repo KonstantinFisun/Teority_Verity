01516e599ae7bc1a704319160e4f585660213afc
KonstantinFisun <72182894+KonstantinFisun@users.noreply.github.com>
2021-05-29 22:42:16 +0300

    Скинул первую версию

---
+++ Teority_Verity/Теор вер/Тест по теории вероятности/Тест по теории вероятности/IDS_class_with_image.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Тест_по_теории_вероятности
+{
+    class IDS_class_with_image
+    {
+
+
+
+    }
+}
